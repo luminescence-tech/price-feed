47ce119ded25a76b7d723918a31338048247aa8a
b-xenon <xenon.le09@gmail.com>
2023-04-25 12:37:44 +0300

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,27 +2,20 @@ require('@nomicfoundation/hardhat-toolbox');
 require('dotenv').config()
 
 module.exports = {
-	solidity: {
-		version: "0.8.19",
-		settings: {
-			optimizer: {
-				enabled: true
-			}
-		},
-		allowUnlimitedContractSize: true,
-		networks: {
-			hardhat: {},
-			ETH_MAINNET: {
-				accounts: [`${process.env.PRIVATE_KEY}`],
-				url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
-			},
-			goerli: {
-				accounts: [`${process.env.PRIVATE_KEY}`],
-				url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
-			}
-		},
-		etherscan: {
-			apiKey: `${process.env.ETHERSCAN_API_KEY}`
+	solidity:"0.8.19",
+	allowUnlimitedContractSize: true,
+	networks: {
+		hardhat: {},
+		// ETH_MAINNET: {
+		// 	accounts: [`${process.env.PRIVATE_KEY}`],
+		// 	url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+		// },
+		goerli: {
+			accounts: [`0xfa4feaa8e7958c3ec039dc048cc0378e76738e4e215cdae4e312940d38982533`],
+			url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
 		}
+	},
+	etherscan: {
+		apiKey: `${process.env.ETHERSCAN_API_KEY}`
 	}
 }
---
+++ scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const Lock = await ethers.getContractFactory("BlizzardOracleRouter");
+  const lock = await Lock.deploy();
+
+  await lock.deployed();
+
+  console.log(
+    `Lock with ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
