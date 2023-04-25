b02904a3a4076f8b5954dc2ed901bc542813de55
b-xenon <xenon.le09@gmail.com>
2023-04-25 15:57:07 +0300

    clean cached

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
 	networks: {
 		hardhat: {},
 		// ETH_MAINNET: {
-		// 	accounts: [`${process.env.PRIVATE_KEY}`],
+		// 	accounts: [],
 		// 	url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
 		// },
 		goerli: {
-			accounts: [`0xfa4feaa8e7958c3ec039dc048cc0378e76738e4e215cdae4e312940d38982533`],
+			accounts: [`0x${process.env.PRIVATE_KEY}`],
 			url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
 		}
 	},
